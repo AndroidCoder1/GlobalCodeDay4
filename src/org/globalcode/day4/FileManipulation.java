4302e7e9f40a9f39fb16bea0e0a784148c4f16ec
AndroidCoder1 <lisa.owusu@psyphertxt.com>
2019-08-01 14:04:48 +0000

    Creating methods of functionalities

--- src/org/globalcode/day4/FileManipulation.java
+++ src/org/globalcode/day4/FileManipulation.java
@@ -8,13 +8,24 @@ import java.util.*;
 
 public class FileManipulation {
 
-    static String contents = "";
-
     public static void main(String[] args) {
 
+        FileManipulation fm = new FileManipulation();
         //Specifying Path to file
         Path path = Paths.get("fp.txt");
 
+        String contents = fm.readFromLocalFile(path);
+
+        System.out.println("File Contents::::"+contents);
+
+        System.out.println("Number of Words in File::::"+countWordsInString(contents));
+        System.out.println("Number of Words more than 5 characters::::"+countWordsMoreThan5Characters(contents));
+
+        //Printout of items and their frequencies
+        createHistogram(contents);
+    }
+
+    private String readFromLocalFile(Path path) {
         //Creating try with resource to prevent exceptions
         try(BufferedReader reader = Files.newBufferedReader(path)) {
 
@@ -24,23 +35,12 @@ public class FileManipulation {
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
             }
-            contents = stringBuilder.toString();
-
-            System.out.println("File Contents::::"+contents);
-
-            System.out.println("Number of Words in File::::"+countWordsInString(contents));
-            System.out.println("Number of Words more than 5 characters::::"+countWordsMoreThan5Characters(contents));
-
-            //Printout of items and their frequencies
-            createHistogram(contents);
+            return stringBuilder.toString();
 
         } catch (Exception e){
             e.printStackTrace();
         }
-
-
-
-
+        return null;
     }
 
     private static int countWordsInString(String content){
@@ -65,17 +65,13 @@ public class FileManipulation {
         for(String word : words){
             counts.add(word.length());
         }
+
         System.out.println(counts);
 
         HashSet<Integer> distinctSet = new HashSet<>(counts);
         for (int number : distinctSet){
-            int occurence = Collections.frequency(counts, number);
-            System.out.println(number + ": " + occurence);
+            int occurrence = Collections.frequency(counts, number);
+            System.out.println(number + ": " + occurrence);
         }
-
-
-
     }
-
-
 }
